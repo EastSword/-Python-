87215be82606572acd6133f32423486682f75b95
EastSword <115763955+EastSword@users.noreply.github.com>
2023-03-06 22:56:15 +0800

    Create 剪枝.py

---
+++ 8.二叉树/遍历类题目/剪枝.py
@@ -0,0 +1,17 @@
+# 题目地址：https://leetcode.cn/problems/pOCWxh/description/
+# 题目描述：给定一个二叉树 根节点 root ，树的每个节点的值要么是 0，要么是 1。请剪除该二叉树中所有节点的值为 0 的子树。节点 node 的子树为 node 本身，以及所有 node 的后代。
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return root
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        if not root.left and not root.right and root.val == 0:
+            return None
+        return root
